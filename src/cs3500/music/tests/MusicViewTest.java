da932abcb47f82d51aae3294959d67877679df79
Walker <walkermayerchak@gmail.com>
2016-03-21 14:37:12 -0400

    added test class

---
+++ src/cs3500/music/tests/MusicViewTest.java
@@ -0,0 +1,15 @@
+package cs3500.music.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Walker on 3/21/2016.
+ */
+public class MusicViewTest {
+  @Test
+  void testDisplay() {
+    return;
+  }
+}
