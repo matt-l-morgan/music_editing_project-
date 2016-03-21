1afac37d5d08cd8adf255c9777b3dec55de4697e
mattmorgan <mlylemorgan@gmail.com>
2016-03-20 20:54:40 -0400

    new model

--- src/cs3500/music/view/ConsoleView.java
+++ src/cs3500/music/view/ConsoleView.java
@@ -5,7 +5,7 @@ import cs3500.music.model.IMusicEditorModel;
 /**
  * Created by mattmorgan on 3/20/16.
  */
-public class ConsoleView extends MusicView{
+public class ConsoleView implements MusicView {
 
   @Override public void display(IMusicEditorModel model) {
 
