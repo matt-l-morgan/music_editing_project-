03f784930757c67ec23fa120be47eb133cfca4fd
mattmorgan <mlylemorgan@gmail.com>
2016-03-22 02:18:41 -0400

    got started on Gui view

--- src/cs3500/music/view/GuiViewGrid.java
+++ src/cs3500/music/view/GuiViewGrid.java
@@ -1,7 +1,14 @@
 package cs3500.music.view;
 
+import cs3500.music.model.MusicEditorModel;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by mattmorgan on 3/22/16.
  */
-public class GuiViewGrid {
+public class GuiViewGrid extends JPanel {
+  private final MusicEditorModel model;
+
 }
