0d1d357d0f9d0c3fdf58707ee80651f47fb8220e
mattmorgan <mlylemorgan@gmail.com>
2016-03-22 22:35:59 -0400

    Main method now takes 2 strings paraments: the first being the peice of music read by the musicreader util, then next being a string representation of the type to view to be played

--- src/cs3500/music/MusicEditor.java
+++ src/cs3500/music/MusicEditor.java
@@ -1,11 +1,16 @@
 package cs3500.music;
 
+
+import cs3500.music.util.CompositionBuilder;
+import cs3500.music.util.MusicReader;
 import cs3500.music.view.ConsoleView;
 import cs3500.music.view.GuiViewFrame;
 import cs3500.music.view.MidiViewImpl;
 import cs3500.music.view.MusicView;
 import cs3500.music.view.MusicViewCreator;
+import cs3500.music.model.*;
 
+import java.io.FileReader;
 import java.io.IOException;
 import javax.sound.midi.InvalidMidiDataException;
 
@@ -17,9 +22,21 @@ public class MusicEditor {
     "console" will produce the text output above, and running it with arguments "mystery-1.txt"
      and "midi" will play the first mystery file via MIDI.
      */
+    CompositionBuilder<IMusicEditorModel> builder = new MusicEditorModel.Builder();
+    MusicReader.parseFile(new FileReader(args[0]), builder);
+    MusicView console_view = MusicViewCreator.create("console");
+    MusicView midi_view = MusicViewCreator.create("midi");
+    MusicView gui_view = MusicViewCreator.create("gui");
+    System.out.print(builder.build().getNotes());
 
-    MusicView view1 = MusicViewCreator.create(System.getenv("viewType"));
-    //readable...
-    //not done
+    if (args[1] == "midi") {
+      midi_view.display(builder.build());
+    }
+    else if (args[1] == "console"){
+      console_view.display(builder.build());
+    }
+    else if (args[1] == "gui") {
+      gui_view.display(builder.build());
+    }
   }
 }
